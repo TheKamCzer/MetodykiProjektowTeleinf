5064aa5f78860b4e2536a64b45d444f751d43f62
Camcore95 <kamilsternal20@wp.pl>
2019-11-30 21:33:24 +0100

    Code refactor

--- QPSK/Demodulator.py
+++ QPSK/Demodulator.py
@@ -9,28 +9,20 @@ class Demodulator:
         self.fi = fi
         self.sampleRate = sampleRate
         self.numOfPeriods = numOfPeriods
+        self.firFilter = signal.firwin(int(self.symbolLength / 2) - 1 , self.carrierFreq / self.sampleRate)
+        self.filterDelay = signal.lfilter_zi( self.firFilter, 1)
 
     def demodulate(self, inputSignal):
         sigLen = int(len(inputSignal))
         t = numpy.linspace(0, self.numOfPeriods * sigLen / self.carrierFreq / self.symbolLength, sigLen)
-        fi = 2 * numpy.pi * self.carrierFreq * t + self.fi
-        branchQ = inputSignal * numpy.sin(fi)
-        branchI = inputSignal * numpy.cos(fi)
-        firFilter = signal.firwin(int(self.symbolLength / 2) - 1 , self.carrierFreq / self.sampleRate, pass_zero=True)
-        filterDelay = signal.lfilter_zi(firFilter, 1)
+        phase = 2 * numpy.pi * self.carrierFreq * t + self.fi
+        branchQ = inputSignal * numpy.sin(phase)
+        branchI = inputSignal * numpy.cos(phase)
         result = []
         for i in range(int(len(inputSignal) / self.symbolLength)):
-            signalQ, _ = signal.lfilter(firFilter, 1, branchQ[i * self.symbolLength : (i + 1) * self.symbolLength], zi=filterDelay)
-            signalI, _ = signal.lfilter(firFilter, 1, branchI[i * self.symbolLength : (i + 1) * self.symbolLength], zi=filterDelay)
+            signalQ, _ = signal.lfilter(self.firFilter, 1, branchQ[i * self.symbolLength : (i + 1) * self.symbolLength], zi=self.filterDelay)
+            signalI, _ = signal.lfilter(self.firFilter, 1, branchI[i * self.symbolLength : (i + 1) * self.symbolLength], zi=self.filterDelay)
 
-            bitQ = 1
-            bitI = 1
-
-            if numpy.mean(signalI) <= 0:
-                bitI = -1
-
-            if numpy.mean(signalQ) <= 0:
-                bitQ = -1
-            result.append(bitI)
-            result.append(bitQ)
+            result.append(0 if numpy.mean(signalI) <= 0 else 1)
+            result.append(0 if numpy.mean(signalQ) <= 0 else 1)
         return result

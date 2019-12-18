60724f1bce42f9ae95c55c7167f4764050ed82aa
kbie1 <48415675+kbie1@users.noreply.github.com>
2019-12-18 23:52:59 +0100

    Update BufferIn.py

--- Buffers/BufferIn.py
+++ Buffers/BufferIn.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def createBufferIn(self, data):
 bufferin = np.unpackbits(data).flatten()
- datain = np.frombuffer(bufferin, dtype='S1')
+ datain = np.frombuffer(bufferin, dtype='S1', count=1024) #Na przykład 1024, wielkość ramki bez nagłówków
 if len(datain) % 2 != 0:
             raise Exception('Data is uneven!')
             returnVal = []

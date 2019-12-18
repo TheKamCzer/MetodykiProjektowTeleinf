659f1da48d228f6d6e15f5016465379c581ec161
kbie1 <48415675+kbie1@users.noreply.github.com>
2019-12-18 23:32:29 +0100

    Create BufferIn.py

---
+++ Buffers/BufferIn.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+
+def createBufferIn(self, data):
+bufferin = np.unpackbits(data).flatten()
+ dataout = np.frombuffer(bufferin, dtype='S1')
+if len(dataout) % 2 != 0:
+            raise Exception('Data is uneven!')
+            returnVal = []
+            return ReturnVal

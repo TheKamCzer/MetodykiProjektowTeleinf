3051003221795755c7e8d0c8afeaeaa5e6f5a7d7
kbie1 <48415675+kbie1@users.noreply.github.com>
2019-12-18 23:41:48 +0100

    Update and rename BufferOut.py to BufferIn.py

--- Buffers/BufferIn.py
+++ Buffers/BufferIn.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 
-def createBufferOut(self, data):
+def createBufferIn(self, data):
 bufferin = np.unpackbits(data).flatten()
  dataout = np.frombuffer(bufferin, dtype='S1')
 if len(dataout) % 2 != 0:

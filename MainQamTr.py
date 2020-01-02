119ec5c885e3a9a7258b513cbac9c0194b1aca1b
Pawel Wozny <pawel0wozny@gmail.com>
2020-01-02 23:02:10 +0100

    QAM: added simple hw loop

--- MainQamTr.py
+++ MainQamTr.py
@@ -7,14 +7,24 @@ from Record_Play.Recorder import Recorder
 from QAM.ModulatorQAM import ModulatorQAM
 from time import sleep
 
+from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 
-# /usr/local/lib/python3.7/dist-packages
-# /usr/local/lib/python3.7/site-packages
-# TODO: Create asserts that checks whether iio lib is installed
-import iio
+# TODO: Create asserts that checks whether iiolib and pyadi is installed
+import adi
 
+# Create instance of PLUTO radio and configure its properties
+sdr = adi.Pluto()
+# sdr.tx_rf_bandwidth = 4000000
+sdr.tx_lo = int(2e9) #2000000000
+# TODO: remove cyclic buffer as it continously sends single packet 
+sdr.tx_cyclic_buffer = True 
+sdr.tx_hardwaregain = -10
+sdr.gain_control_mode = "slow_attack"
+
+fs = int(sdr.sample_rate)
+print("PLUTO sample rate: " + str(fs))
 
 # Recorder class is used to open input device (microphone)
 rec = Recorder(input_device_index=9, frames_per_buffer=1024,
@@ -23,7 +33,7 @@ rec = Recorder(input_device_index=9, frames_per_buffer=1024,
 # Modulator class transforms microphone data into QAM modulated packets
 # TODO: calculate correct sample rate which is
 # sum of data rate(bit_rate) + overhead created by header and footer
-mod = ModulatorQAM(carrierFreq=100e3, upsamplingFactor=8, sampleRate=20e6)
+mod = ModulatorQAM(carrierFreq=100e3, upsamplingFactor=8, sampleRate=fs)
 
 # Starting gathering samples. Data is returned by method get_data()
 rec.start()
@@ -37,9 +47,32 @@ while True:
 
         # TODO: add proper header and trailer to data
         modulatedFrame = mod.modulateQAM16(
-            soundFrame, isSignalUpconverted=True, debug=True)
+            soundFrame, isSignalUpconverted=True, debug=False)
         print(modulatedFrame[:100])
         # plt.plot(np.real(modulatedFrame))
         # plt.show()
+
+        # TODO: remove cyclic buffer and feed data continuously 
+        sdr.tx(modulatedFrame)
+        break
+
+
+for _ in range(200):
+    x = sdr.rx()
+    f, Pxx_den = signal.periodogram(x, fs)
+    plt.clf()
+    plt.semilogy(f, Pxx_den)
+    plt.ylim([1e-7, 1e2])
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("PSD [V**2/Hz]")
+    plt.draw()
+    plt.pause(0.05)
+    sleep(0.1)
+
+plt.show()
+
+
+
+
 
         

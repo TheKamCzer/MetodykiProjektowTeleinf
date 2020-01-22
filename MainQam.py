09cf26e4458428639b0c315522e0999070ee975a
Pawel Wozny <pwozny@cern.ch>
2020-01-22 15:18:42 +0100

    QAM: overhaul of main QAM

--- MainQam.py
+++ MainQam.py
@@ -2,136 +2,121 @@
 """
 Code for receiver and transmitter used for QAM modulation
 usage:
-    python3 MainQam.py --function tx 
-    python3 MainQam.py --function rx 
+    python3 MainQam.py --function tx
+    python3 MainQam.py --function rx
 Information about other options is avaliable by:
     python3 MainQam.py -h
 """
-# TODO: pass those parameters from command line
-__SAMPLING_RATE = 20e6
-__CARRIER_FREQ = 10e5
+from Record_Play.Recorder import Recorder
+from QAM.ModulatorQAM import ModulatorQAM
+from time import sleep
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+import adi
+import random
+import itertools
 
-__START_HEADER = [0,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,
-                 2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(+15)+1, 0]
-__END_HEADER = [1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,
-               2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(+15)+1,2**(+15)+1,2**(-15)-1,2**(-15)-1,2**(+15)+1, 0]
 
-def main_qam_tx(debug: bool = False, mic_used: bool = False, hw_used: bool = False, dry_run: bool = True):
-    from Record_Play.Recorder import Recorder
-    from QAM.ModulatorQAM import ModulatorQAM
-    from time import sleep
-    if debug == True:
-        from scipy import signal
-        import numpy as np
-        import matplotlib.pyplot as plt
+class NieQam:
 
-    if dry_run == True:
-        hw_used = False
-        mic_used
-    if hw_used == True:
-        try:
-            import adi
-            if debug == True:
-                print("ADI PLUTO lib successfully imported.")
+    def __init__(self, debug: bool = False, hw_used: bool = False):
 
-            sdr = adi.Pluto()
-            # TODO: Change to int as conversion introduces rounding errors
-            sdr.tx_lo = int(2e9)  # 2000000000
-            # TODONE: remove cyclic buffer as it continously sends single packet/framebuffer
-            sdr.tx_cyclic_buffer = False
-            sdr.tx_hardwaregain = -10
-            sdr.gain_control_mode = "slow_attack"
-            fs = int(sdr.sample_rate)
-            if debug == True:
-                print("PLUTO sample rate: " + str(fs))
-        except:
-            print('Error while importing Analog Device\'s PLUTO library\n')
+        # Gold sequence #1, 32
+        self.__START_HEADER_QAMA = [0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3,
+                                    0, 0, 0, 0, 0, 3, 0, 0, 3, 3, 3, 0, 3, 3, 0, 0, 0, 3, 3, 0]
+        self.__START_HEADER_QAMA = [[0, 0] if x == 0 else [1, 1]
+                                    for x in self.__START_HEADER_QAMA]
+        self.__START_HEADER_QAMA = list(
+            itertools.chain.from_iterable(self.__START_HEADER_QAMA))
+        # Pseudo random binary sequence
+        __STOP_HEADER_QAMA = [3, 3, 3, 0, 0, 0, 3, 0, 0, 3, 3, 0, 3, 0, 3, 3, 3, 0, 0,
+                              0, 3, 0, 0, 3, 3, 0, 3, 0, 3, 3, 3, 3, 0, 0, 0, 3, 0, 0, 3, 3, 0, 3, 0, 3]
+        self.__STOP_HEADER_QAMA = [[0, 0] if x ==
+                                   0 else [1, 1] for x in __STOP_HEADER_QAMA]
+        self.__STOP_HEADER_QAMA = list(
+            itertools.chain.from_iterable(__STOP_HEADER_QAMA))
 
-    if mic_used == True:
-        try:
+        self.modulatedHeader = mod.modulateQAMA(__START_HEADER_QAMA)
+        self.modulatedFooter = mod.modulateQAMA(__STOP_HEADER_QAMA)
+
+
+        # asdasdasdasdasdasd
+        self.__SAMPLING_RATE = 20e6
+        self.__CARRIER_FREQ = 10e5
+
+        self.debug = debug
+        self.hw_used = hw_used
+
+        self.mod = ModulatorQAM(carrierFreq=self.__CARRIER_FREQ,
+                           upsamplingFactor=8, sampleRate=self.__SAMPLING_RATE)
+
+        if self.hw_used == True:
+            self.sdr = adi.Pluto()
+            self.sdr.tx_lo = int(2e9)  # 2000000000
+            self.sdr.tx_cyclic_buffer = False
+            self.sdr.tx_hardwaregain = -10
+            self.sdr.gain_control_mode = "slow_attack"
+            self.fs = int(sdr.sample_rate)
+            print("PLUTO sample rate: " + str(fs))
+
             # Recorder class is used to open input device (microphone)
-            # TODO: automatically find correct `input_device_index`
-            rec = Recorder(input_device_index=1, frames_per_buffer=1024,
+            self.rec = Recorder(input_device_index=1, frames_per_buffer=1024,
                            channels=1, bit_rate=44100)
-            if debug == True:
-                rec.get_mic_info()
-            rec.start()
-            # TODO: caught exception when bad index is chosen
-            # INFO: for demonstration, mic is turned off
-        except:
-            print('Ex: error while opening input stream\n')
-
-    # Modulator class transforms microphone data into QAM modulated packets
-    # TODO: calculate correct sample rate which is
-    # sum of data rate(bit_rate) + overhead created by header and footer
-    mod = ModulatorQAM(carrierFreq=__CARRIER_FREQ,
-                       upsamplingFactor=8, sampleRate=__SAMPLING_RATE)
-
-    if mic_used == True and hw_used == True:
-        # Starting gathering voice samples. Data is returned by method get_data()
-
-        try:
-            print('Transmission Started!')
-            while True:
-                if rec.isEmpty():
-                    # sleep time related to acq of single voice frame
-                    sleep(44100/1024)
-                else:
-                    # obtaining samples from microphone
-                    soundFrame = rec.get_data()
-
-                    # modulating data
-                    # TODO: add proper header and trailer to data
-                    modulatedFrame = mod.modulateQAM16(
-                        soundFrame, isSignalUpconverted=True, debug=debug)
-
-                    # send data to pluto
-                    sdr.tx(modulatedFrame)
-
-        except KeyboardInterrupt:
-            print('Transmission Ended!')
-            rec.exit()
-
-    elif debug == True:
-        try:
-            from QAM_UT.ModulatorQAM_UT import __RANDOM_1024_INT16 as soundFrame
-            # from Synchronization_ST.FrameSynchronization_ST import __START_HEADER
-            # from Synchronization_ST.FrameSynchronization_ST import __END_HEADER
-
-            print("Dry run. Showing plots...")
-            while True:
-                modulatedFrame = mod.modulateQAM16(
-                    np.concatenate((np.frombuffer(np.asarray(__START_HEADER) * 2**15,  dtype=np.int16),
-                                   soundFrame[:], # TODO: change number of samples by cmd option
-                                   np.frombuffer(np.asarray(__END_HEADER)* 2**15,  dtype=np.int16))),
-                    isSignalUpconverted=True, debug=True)
-                
-                break;
-
-        except KeyboardInterrupt:
-            print('Dry Run Ended!')
-
-        # TODO: show just plots
-
-        print(modulatedFrame[:100])
-        # plt.plot(np.real(modulatedFrame))
-        # plt.show()
-
-        pass
-    else:
-        NameError("asdasasd")
+            self.rec.get_mic_info()
+            self.rec.start()
 
 
-def main_qam_rx(debug: bool = False):
-    from Record_Play.Player import Player
-    from QAM.DemodulatorQAM import DemodulatorQAM
-    from time import sleep
-    if debug == True:
-        from scipy import signal
-        import numpy as np
-        import matplotlib
-    
-    print("Starting demodulator")
+
+    def main_qam_tx(self, mic_port: int = 0):
+
+        if self.hw_used == True:
+            try:
+
+                print('Transmission Started!')
+                while True:
+                    if self.rec.isEmpty():
+                        # sleep time related to acq of single voice frame
+                        sleep(44100/1024)
+                    else:
+                        # obtaining samples from microphone
+                        soundFrame = rec.get_data()
+
+                        modulatedFrame = self.mod.modulateQAMA(
+                            self.modulatedHeader + soundFrame + self.modulatedFooter, debug=True)
+
+                        # send data to pluto
+                        self.sdr.tx(modulatedFrame)
+
+            except KeyboardInterrupt:
+                print('Transmission Ended!')
+                self.rec.exit()
+
+        else:
+            try:
+
+                print("Dry run. Showing plots...")
+                while True:
+                    __SIMULATION_DATA = [
+                        random.randint(0, 1) for x in range(1024)]
+                    modulatedFrame = self.mod.modulateQAMA(
+                        self.modulatedHeader + __SIMULATION_DATA + self.modulatedFooter, debug=True)
+
+            except KeyboardInterrupt:
+                print('Dry Run Ended!')
+            pass
+
+    def main_qam_rx(self, debug: bool = False):
+        from Record_Play.Player import Player
+        from QAM.DemodulatorQAM import DemodulatorQAM
+        from time import sleep
+        if debug == True:
+            from scipy import signal
+            import numpy as np
+            import matplotlib
+
+        print("Starting demodulator")
+
 
 if __name__ == "__main__":
     import argparse
@@ -151,6 +136,7 @@ if __name__ == "__main__":
 
     if vars(args)['function'] == 'tx':
         main_qam_tx(vars(args)['no_debug'], vars(args)[
-                    'mic_used'], vars(args)['hw_used'],  vars(args)['dry_run'])
+                    'mic_used'], vars(args)['hw_used'])
+
     elif vars(args)['function'] == 'rx':
         main_qam_rx(vars(args)['debug'])

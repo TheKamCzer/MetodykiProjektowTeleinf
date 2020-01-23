eed4c414fa52e4f5fb72f6aeaf9589a8049381a7
Pawel Wozny <pwozny@cern.ch>
2020-01-23 09:45:40 +0100

    qam: added reception based on asyncio

--- QAM/Reception.py
+++ QAM/Reception.py
@@ -5,7 +5,7 @@
 """
 from rtlsdr import RtlSdr
 import threading
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from queue import Queue
 import numpy as np
 import commpy as cp
@@ -14,9 +14,33 @@ import pyaudio
 import asyncio
 from time import sleep
 
+from Synchronization.FrameSynchronization import FrameSynchronization
+from Synchronization.TimingRecovery import TimingRecovery
+
+from Transmission import Transmitter
+
+
 class Reception:
 
     def __init__(self, lo_frequency=1.5e9, sampling_rate=1e6, upsamplingFactor=2, output_device_index=0):
+
+        # frame modulation #
+        # Gold sequence #1, 32
+        # TOTAL SIZE: 64 bits
+        self.packet_header = [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+        # Pseudo random binary sequence
+        # TOTAL SIZE: 88 bits
+        self.packet_footer = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+                              1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
+        self._MAPPING_TABLE_QAM4 = {
+            (0, 0): -1 - 1j,
+            (0, 1): 1 - 1j,
+            (1, 0): -1 + 1j,
+            (1, 1): +1 + 1j
+        }
+        self.psfFilter = cp.rrcosfilter(int(self.upsamplingFactor) * 10, 0.35,
+                                        self.upsamplingFactor / self.sampling_rate, self.sampling_rate)[1]                    
 
         # Constructor data #
         self.lo_frequency = lo_frequency
@@ -30,13 +54,18 @@ class Reception:
         # Queue for playing data
         self.audioQueue = Queue(22)
 
+        # Since maximimum number of samples that RTLSDR can return is 131072
+        # we do not need concatenate more than last output buffer
+        self.lastBuffer= [0]
+
+
 
         # RTL
-        # self.rtl = RtlSdr()
-        # self.rtl.sample_rate = self.sampling_rate
-        # self.rtl.center_freq = self.lo_frequency
-        # self.rtl.freq_correction = 60   # PPM
-        # self.rtl.gain = 'auto'
+        self.rtl = RtlSdr()
+        self.rtl.sample_rate = self.sampling_rate
+        self.rtl.center_freq = self.lo_frequency
+        self.rtl.freq_correction = 60   # PPM
+        self.rtl.gain = 'auto'
 
         # Player
         self.p = pyaudio.PyAudio()
@@ -47,21 +76,38 @@ class Reception:
             stream_callback=self.player_callback)
 
         
+        # Data recovery 
+        self.frameSync = FrameSynchronization(self.recreate_header_frame(self.packet_header),
+                                              self.recreate_header_frame(self.packet_footer),
+                                              self.upsamplingFactor, self.sampling_rate)
+
+        self.timeRecover = TimingRecovery(self.upsamplingFactor)
+
+        
         def player_callback(self, in_data, frame_count, time_info, status):
             if self.audioQueue.empty():
-                data =[0]*1024*4
+                data =[0, 1]*1024*4
             else:
                 data = self.audioQueue.get()
+                self.audioQueue.task_done()
+
+            return (data, pyaudio.paContinue)
 
 
 
 
     async def rtl_streaming(self):
+        dataCount= 0
+
         async for samples in self.rtl.stream():
             # do something with samples
             # ...
             print(samples)
             print(len(samples))
+            self.inputQueue.put([dataCount, "Asdasdasdasd"])
+            print("data pushed to queue", str(dataCount))
+            dataCount = dataCount +1
+
         # to stop streaming:
         await self.rtl.stop()
 
@@ -79,12 +125,67 @@ class Reception:
 
     
     async def data_processing(self):
-        pass
+
+        while True:
+            if not self.inputQueue.empty():
+                
+                raw_input = self.inputQueue.get()
+                self.inputQueue.task_done()
+
+                samples_to_process= np.concatenate(self.lastBuffer, raw_input).ravel()
+                samples_corrected_timing = self.frameSync.correctFreqAndPhase(samples_to_process)
+                dataPosition = self.frameSync.synchronizeStartHeader(samples_corrected_timing)
+                dataEndPosition = self.frameSync.synchronizeStopHeader(samples_corrected_timing)
+                output_frame = samples_corrected_timing[dataPosition:dataEndPosition]
+                data = self.timeRecover.synchronizeTiming(output_frame)
+
+                bits = np.packbits(data.view(np.int8))
+                self.audioQueue.put(bits)
+                self.lastBuffer = raw_input
+
+                print(data[:10*8])
+                print(bits[:10])
+
+                fig,ax = plt.subplots()
+                ax.scatter(data.real,data.imag)
+                plt.show(block=True)
 
 
-    async def data_player(self):
-        pass
 
+
+
+
+
+
+
+    def recreate_header_frame(self, data_to_send):
+
+        # QAM4 has 2 bits per symbol thus we divide by 2
+        Bits_per_symbol = 2
+        symbol_length = int(len(data_to_send)/Bits_per_symbol)
+
+        # grouping data into chunks of 2
+        data_groupped = np.array(data_to_send).reshape(
+            symbol_length, Bits_per_symbol)
+
+        # using dictionary to convert input data array to symbols
+        symbols_QAM4 = np.array(
+            [self._MAPPING_TABLE_QAM4[tuple(b)] for b in data_groupped])
+
+        # Upsample the data
+        signalIus = signal.upfirdn([1], np.real(
+            symbols_QAM4), self.upsamplingFactor)
+        signalQus = signal.upfirdn([1], np.imag(
+            symbols_QAM4), self.upsamplingFactor)
+
+        # Filter and remove spurious samples added by convolution
+        filteredI = np.convolve(signalIus, self.psfFilter)
+        filteredQ = np.convolve(signalQus, self.psfFilter)
+
+        signalI = filteredI[int(self.upsamplingFactor * 5)  : - int(self.upsamplingFactor * 5) + 1]
+        signalQ = filteredQ[int(self.upsamplingFactor * 5)  : - int(self.upsamplingFactor * 5) + 1]
+
+        return signalI + 1j*signalQ
 
 
 
@@ -96,51 +197,12 @@ class Reception:
 rx = Reception()
 
 loop = asyncio.get_event_loop()
-# loop.run_until_complete(rx.rtl_streaming())
-loop.run_until_complete(rx.simulation_stream())
+loop.create_task(rx.rtl_streaming())
+loop.create_task(rx.data_processing())
 
+loop.run_forever()
+loop.close()
 
-#         aaa = self.rtl.read_samples(2920)
-#         print(aaa)
-
-#         # self.packetQueue = Queue(10)
-    
-#     def receive(self):
-#         print(self.rtl.read_samples(32920))
-
-# rx = Reception()
-# # while True:
-# # rx.receive()
-
-
-# from time import sleep
-
-
-# import asyncio
-# from rtlsdr import RtlSdr
-
-# async def streaming():
-#     sdr = RtlSdr()
-#     sdr.sample_rate =1e6
-#     sdr.center_freq = 1.5e9
-
-#     async for samples in sdr.stream():
-#         # do something with samples
-#         # ...
-#         print(samples)
-#         print(len(samples))
-
-#     # to stop streaming:
-#     sleep(2)
-#     await sdr.stop()
-
-#     # done
-#     sdr.close()
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(streaming())
-
-# print("asdas")
 
 
 
